0bf9c775977ce9399eaacf6b17f4f7652c3154f5
MarcyMarcy <ironsalsa@gmail.com>
2018-10-02 16:34:13 -0600

    Create cmd.sh

---
+++ cmd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Start conversion, grabbing all params given
+echo "Starting conversion..."
+/usr/bin/mitmdump $@
+
+echo "Conversion complete - reporting to EtcD."
+
+# Reporting in to EtcD that our conversion is no longer running ("false") and set another endpoint to blank
+# Customize the below to whatever you need to deliver status to EtcD - this is just an example
+
+etcdctl put $CONVERTRUNNING_ETCD_ENDPOINT false
+etcdctl put $CONVERTNAME_ETCD_ENDPOINT ""
+
+exit 0
